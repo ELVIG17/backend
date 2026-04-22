1f24b23ff3eb160360a7f2e4b5ea7cf11ae61270
ELVIG17 <elvig.shakirov@gmail.com>
2026-04-22 07:24:13 +0000

    REMAKE SERVER TS

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,33 @@
-import express, { type Request, type Response } from "express";
+import "dotenv/config";
+import express from "express";
 import cors from "cors";
-import postsRouter from "./api/posts"
-// import { pool } from "./db";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./api/auth";
-import cookieParser from "cookie-parser"
+import postsRouter from "./api/posts";
+import messagesRouter from "./api/messages";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
 
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-app.use("/api/auth", authRouter);
-
-app.use("/posts", postsRouter);
-
-
-
-app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok!!!!!!" });
+app.get("/", (_req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
-const PORT = 3000;
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/messages", messagesRouter);
 
+const PORT = Number(process.env.PORT ?? 3000);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
